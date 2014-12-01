df376717b47da0f70fd0189422a710bd534243ab
Jonathan Clem <jonathan@jclem.net>
2014-12-01 16:03:32 -0500

    use async for less synchronous migrations

--- bin/sql-migrate.js
+++ bin/sql-migrate.js
@@ -2,60 +2,60 @@
 
 'use strict';
 
-var fs      = require('fs');
-var path    = require('path');
 var program = require('commander');
-var lib     = require ('../lib/index.js');
-var version = getVersion();
+var lib     = require ('../lib');
+var version = require('../package.json').version;
 
 program
   .version(version)
-  .option('-d, --database [name]', 'the database to use. if not provided, database defaults to process.env.DATABASE_URL')
-  .option('-m, --migration-dir [dir]', 'the relative location of the migrations directory. defaults to ./migrations');
+  .option('-d, --database [name]', 'the database to use. if not provided, ' +
+          'database defaults to process.env.DATABASE_URL')
+  .option('-m, --migration-dir [dir]', 'the relative location of the ' +
+          'migrations directory. defaults to ./migrations');
 
 program
   .command('create <name>')
   .description('create a new migration')
-  .action(function(name) {
+  .action(function create(name) {
     return lib.create(name, getMigrationDir(program));
   });
 
 program
   .command('up [steps]')
   .description('migrate up')
-  .action(function(steps) {
-    var databaseURL;
+  .action(function up(steps) {
+    var databaseURL = getDatabaseURL(program);
 
-    try {
-      databaseURL = getDatabaseURL(program);
-    } catch(err) {
-      return console.error('\n  error: database not provided or not detected\n');
+    if (!databaseURL) {
+      console.error('Error: Database not provided or not detected.');
+      process.exit(1);
     }
 
     steps = steps === undefined ? steps : +steps;
-    return lib.change(steps, 'up', databaseURL);
+    return lib.change(+steps, 'up', databaseURL, getMigrationDir(program));
   });
 
 program
   .command('down [steps]')
   .description('migrate down')
-  .action(function(steps) {
-    var databaseURL;
+  .action(function down(steps) {
+    var databaseURL = getDatabaseURL(program);
 
-    try {
-      databaseURL = getDatabaseURL(program);
-    } catch(err) {
-      return console.error('\n  error: database not provided or not detected\n');
+    if (!databaseURL) {
+      console.error('Error: Database not provided or not detected.');
+      process.exit(1);
     }
 
     steps = steps === undefined ? 1 : +steps;
-    return lib.change(steps, 'down', databaseURL);
+    return lib.change(steps, 'down', databaseURL, getMigrationDir(program));
   });
 
 program.parse(process.argv);
 
 function getDatabaseURL(program) {
-  if (program.database) {
+  if (program.database && /^postgres:\/\/.+/.test(program.database)) {
+    return program.database;
+  } else if (program.database) {
     return 'postgres://localhost:5432/' + program.database;
   } else {
     return process.env.DATABASE_URL;
@@ -64,10 +64,4 @@ function getDatabaseURL(program) {
 
 function getMigrationDir(program) {
   return program.migrationDir || './migrations';
-}
-
-function getVersion() {
-  var packagePath = path.join(__dirname, '../package.json');
-  var contents    = fs.readFileSync(packagePath);
-  return JSON.parse(contents).version;
 }
